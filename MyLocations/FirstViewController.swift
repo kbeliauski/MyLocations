7253cbde0d6990de23b0a9d525790a5266175280
kbeliauski <bkotutby1112@icloud.com>
2020-07-26 22:38:11 -0400

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Kiryl Beliauski on 7/26/20.
+//  Copyright © 2020 Kiryl Beliauski. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Kiryl Beliauski on 7/26/20.
+//  Copyright © 2020 Kiryl Beliauski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
